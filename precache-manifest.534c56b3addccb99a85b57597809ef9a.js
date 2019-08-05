6aba7fd275ef7d24ef990b63c36858547a2ac88f
Julian <shomalij@msu.edu>
2019-08-05 18:19:31 -0400

    Updates

---
+++ precache-manifest.534c56b3addccb99a85b57597809ef9a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "21abe8849003cb0dd7e1b0ca356df055",
+    "url": "/personal-website/static/media/siblings.21abe884.jpeg"
+  },
+  {
+    "revision": "0d9d4569ac14761da6f9ddc31f63484b",
+    "url": "/personal-website/static/media/julianheadshot.0d9d4569.png"
+  },
+  {
+    "revision": "4a443f5ec0646652ac907dae4541d252",
+    "url": "/personal-website/static/media/grizzhacks2018.4a443f5e.jpg"
+  },
+  {
+    "revision": "4c989847de7497d7851e086f1e3ac636",
+    "url": "/personal-website/static/media/country-image.4c989847.jpg"
+  },
+  {
+    "revision": "d825fe64bac6ffd8700ce6bf05666b20",
+    "url": "/personal-website/static/media/JulianShomaliResumeSS19.d825fe64.pdf"
+  },
+  {
+    "revision": "b6d8e3a721968ec40bad",
+    "url": "/personal-website/static/js/runtime~main.93f2c6c9.js"
+  },
+  {
+    "revision": "67416cdd5ca57794dd45",
+    "url": "/personal-website/static/js/main.476b7bca.chunk.js"
+  },
+  {
+    "revision": "f0a5ea9cb900b1887bd6",
+    "url": "/personal-website/static/js/2.79b22e70.chunk.js"
+  },
+  {
+    "revision": "67416cdd5ca57794dd45",
+    "url": "/personal-website/static/css/main.65de8990.chunk.css"
+  },
+  {
+    "revision": "35dd1229a76424921ff8b7f13b6849ee",
+    "url": "/personal-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30a9fc48ffb579feb035e5256be8db2b.js"
+  "/personal-website/precache-manifest.534c56b3addccb99a85b57597809ef9a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/personal-website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
